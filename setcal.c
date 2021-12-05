b1b4e7dcf0423de9eb8c3c01a5287e6ea0af78b2
Rumburak22 <95107887+Rumburak22@users.noreply.github.com>
2021-12-05 17:53:30 +0100

    Oprava realloc, seřazení

--- setcal.c
+++ setcal.c
@@ -103,6 +103,53 @@ void card(set *a){
     printf("Number of elements is: %d\n", i);
 }
 
+//tiskne průnik množin A a B
+void intersect(set *a,set *b)
+{
+    for(int i=0;i<a->len;i++)
+    {
+        for(int j=0;j<b->len;j++)
+        {
+            if(a->items[i]!=b->items[j])
+            {
+                for(int k=i;k<a->len-1;k++)
+                {
+                    a->items[i]=a->items[i+1];
+                }
+                realloc(a, sizeof(a));
+            }            
+        }
+    }
+    for(int l = 0; l < a->len; l++)
+    {
+        printf("Intersection of sets is : %s",a->items[l]);
+    }
+}
+
+//tiskne rozdíl množin A \ B
+void minus(set *a,set *b)
+{
+    for(int i=0;i<a->len;i++)
+    {
+        for(int j=0;j<b->len;j++)
+        {
+            if(a->items[i]==b->items[j])
+            {
+                for(int k=i;k<a->len-1;k++)
+                {
+                    a->items[i]=a->items[i+1];
+                }
+                realloc(a, sizeof(a));
+            }            
+        }
+    }
+    for(int l = 0; l < a->len; l++)
+    {
+        printf("Difference of sets is : %s",a->items[l]);
+    }
+
+}
+
 //tiskne true nebo false podle toho, jestli je množina A podmnožinou množiny B
 void subseteq(set *a, set *b){
     bool f = false;
@@ -160,52 +207,6 @@ bool equals(set *a, set *b)
     printf("Sets are equal: true\n");
 }
 
-//tiskne průnik množin A a B
-void intersection(set *a,set *b)
-{
-    for(int i=0;i<a->len;i++)
-    {
-        for(int j=0;j<b->len;j++)
-        {
-            if(a->items[i]!=b->items[j])
-            {
-                for(int k=i;k<a->len-1;k++)
-                {
-                    a->items[i]=a->items[i+1];
-                }
-                realloc(a);
-            }            
-        }
-    }
-    for(int l=0;l<a->len;l++)
-    {
-        printf("Intersection of sets is : %s",a->item[i]);
-    }
-}
-
-//tiskne rozdíl množin A a B
-void difference(set *a,set *b)
-{
-    for(int i=0;i<a->len;i++)
-    {
-        for(int j=0;j<b->len;j++)
-        {
-            if(a->items[i]==b->items[j])
-            {
-                for(int k=i;k<a->len-1;k++)
-                {
-                    a->items[i]=a->items[i+1];
-                }
-                realloc(a);
-            }            
-        }
-    }
-    for(int l=0;l<a->len;l++)
-    {
-        printf("Difference of sets is : %s",a->item[i]);
-    }
-
-}
 ////////// PRIKAZY NAD RELACEMI //////////
 
 //tiskne true nebo false, jestli je relace reflexivní.
